1ad59f2e84f9598e3811d6ccea19ebde1798dccb
Axel Rindle <mail@axelrindle.de>
2023-04-10 11:45:44 +0200

    refactor: extract convert command

---
+++ src/commands/convert.ts
@@ -0,0 +1,71 @@
+import { ExecOptions, exec } from 'child_process';
+import { randomBytes } from 'crypto';
+import { basename, dirname, join } from 'path';
+import { FileType, Progress, ProgressLocation, Uri, window, workspace } from 'vscode';
+
+async function doConvert(directory: string, file: string, progress: Progress<any>): Promise<void> {
+	let filename: string = file.split('.')[0];
+	if (!filename) {
+		filename = 'webp-converted-' + randomBytes(4).toString('hex');
+	}
+
+	const fileNew = filename + ".webp";
+	try {
+		const stats = await workspace.fs.stat(Uri.file(join(directory, fileNew)));
+		if (stats.type === FileType.File || stats.type === FileType.SymbolicLink) {
+			const answer = await window.showQuickPick(['Yes', 'No'], {
+				title: `The file ${fileNew} does already exist. Overwrite?`,
+				canPickMany: false,
+				ignoreFocusOut: true,
+			});
+
+			if (answer !== 'Yes') {
+				window.showInformationMessage('WebP convertion canceled.');
+				return;
+			}
+
+			progress.report({ increment: 10 });
+		}
+	} catch (error) {
+		// ignore ¯\_(ツ)_/¯
+	}
+
+	const cmd = `cwebp -preset photo ${directory}/${file} -o ${fileNew}`;
+	const opts: ExecOptions = {
+		cwd: directory,
+		timeout: 30000 // TODO: Make timeout configurable
+	};
+	return new Promise((resolve, reject) => {
+		exec(cmd, opts, error => {
+			if (error) {
+				reject(error);
+			}
+			else {
+				resolve();
+			}
+		});
+	});
+}
+
+export default async function convert(uri: Uri): Promise<void> {
+    const { fsPath } = uri;
+    const directory = dirname(fsPath);
+    const file = basename(fsPath);
+
+    window.withProgress({
+        location: ProgressLocation.Notification,
+        cancellable: false,
+        title: `Converting ${file} into WebP...`
+    }, async (progress) => {
+        try {
+            progress.report({ increment: 10 });
+            await doConvert(directory, file, progress);
+            progress.report({ increment: 100 });
+        } catch (error: any) {
+            window.showErrorMessage(`Failed to convert ${file} into a WebP file!`, {
+                detail: error.message
+            });
+            console.error(error);
+        }
+    });
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,5 @@
-import { exec, ExecOptions } from 'child_process';
-import { randomBytes } from 'crypto';
-import { basename, dirname, join } from 'path';
-import { commands, ExtensionContext, FileType, Progress, ProgressLocation, QuickPickItem, Uri, window, workspace } from 'vscode';
+import { commands, ExtensionContext, ProgressLocation, QuickPickItem, window } from 'vscode';
+import convert from './commands/convert';
 import deleteBinary from './commands/delete-binary';
 import { install, loadVersions } from './downloader';
 import { Version } from './types';
@@ -42,80 +40,11 @@ async function init() {
 	});
 }
 
-async function convert(directory: string, file: string, progress: Progress<any>): Promise<void> {
-	let filename: string = file.split('.')[0];
-	if (!filename) {
-		filename = 'webp-converted-' + randomBytes(4).toString('hex');
-	}
-
-	const fileNew = filename + ".webp";
-	try {
-		const stats = await workspace.fs.stat(Uri.file(join(directory, fileNew)));
-		if (stats.type === FileType.File || stats.type === FileType.SymbolicLink) {
-			const answer = await window.showQuickPick(['Yes', 'No'], {
-				title: `The file ${fileNew} does already exist. Overwrite?`,
-				canPickMany: false,
-				ignoreFocusOut: true,
-			});
-
-			if (answer !== 'Yes') {
-				window.showInformationMessage('WebP convertion canceled.');
-				return;
-			}
-
-			progress.report({ increment: 10 });
-		}
-	} catch (error) {
-		// ignore ¯\_(ツ)_/¯
-	}
-
-	const cmd = `cwebp -preset photo ${directory}/${file} -o ${fileNew}`;
-	const opts: ExecOptions = {
-		cwd: directory,
-		timeout: 30000 // TODO: Make timeout configurable
-	};
-	return new Promise((resolve, reject) => {
-		exec(cmd, opts, error => {
-			if (error) {
-				reject(error);
-			}
-			else {
-				resolve();
-			}
-		});
-	});
-}
-
 export async function activate(context: ExtensionContext) {
 	await init();
 
-	let cmdExecute = commands.registerCommand('webp-converter.execute', async (uri: Uri) => {
-		const { fsPath } = uri;
-		const directory = dirname(fsPath);
-		const file = basename(fsPath);
-
-		window.withProgress({
-			location: ProgressLocation.Notification,
-			cancellable: false,
-			title: `Converting ${file} into WebP...`
-		}, async (progress) => {
-			try {
-				progress.report({ increment: 10 });
-				await convert(directory, file, progress);
-				progress.report({ increment: 100 });
-			} catch (error: any) {
-				window.showErrorMessage(`Failed to convert ${file} into a WebP file!`, {
-					detail: error.message
-				});
-				console.error(error);
-			}
-		});
-	});
-
-	let cmdDeleteBinary = commands.registerCommand('webp-converter.delete-binary', deleteBinary);
-
-	context.subscriptions.push(cmdExecute);
-	context.subscriptions.push(cmdDeleteBinary);
+	context.subscriptions.push(commands.registerCommand('webp-converter.execute', convert));
+	context.subscriptions.push(commands.registerCommand('webp-converter.delete-binary', deleteBinary));
 }
 
 export function deactivate() {}
